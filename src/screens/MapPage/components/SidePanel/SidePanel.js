c54c15add5777f3b8db7a22863ac2f6e0abcb558
Will Granger <twgranger@gmail.com>
2020-07-23 09:42:07 -0500

    Wrap SidePanel in aside

--- src/screens/MapPage/components/SidePanel/SidePanel.js
+++ src/screens/MapPage/components/SidePanel/SidePanel.js
@@ -24,56 +24,59 @@ const StyledText = styled(Text)`
 
 export default function SidePanel() {
   return (
-    <Box
-      background='sand'
-      border={{ color: 'kelp', side: 'right', size: 'xsmall' }}
-      pad='small'
-      width='15rem'
-    >
-      <Box gap='xsmall' margin={{ bottom: 'small' }} width='10rem'>
-        <Image fit='contain' margin={{ right: 'auto' }} src={Logo} />
-        <StyledText color='kelp' size='xlarge'>
-          Falkland Islands
-        </StyledText>
-        <LocationDrop />
-      </Box>
-
+    <aside>
       <Box
-        border={{ color: 'kelp', side: 'horizontal', size: 'xsmall' }}
-        gap='xsmall'
-        pad={{ vertical: 'small' }}
+        background='sand'
+        border={{ color: 'kelp', side: 'right', size: 'xsmall' }}
+        height={{ min: '100%' }}
+        pad='small'
+        width='15rem'
       >
-        <Text color='kelp'>Getting started</Text>
-        <Text size='xsmall'>
-          Use the rectangle tool to draw a box around the
-          area of the map you'd like to learn more about
-        </Text>
-        <StyledButton
-          color='kelp'
+        <Box gap='xsmall' margin={{ bottom: 'small' }} width='10rem'>
+          <Image fit='contain' margin={{ right: 'auto' }} src={Logo} />
+          <StyledText color='kelp' size='xlarge'>
+            Falkland Islands
+          </StyledText>
+          <LocationDrop />
+        </Box>
+
+        <Box
+          border={{ color: 'kelp', side: 'horizontal', size: 'xsmall' }}
           gap='xsmall'
-          hoverIndicator={{ color: 'indiglo' }}
-          icon={<Image src={RectangleIcon} />}
-          label={<Text size='xsmall'>RECTANGLE TOOL</Text>}
-          plain
-        />
-      </Box>
+          pad={{ vertical: 'small' }}
+        >
+          <Text color='kelp'>Getting started</Text>
+          <Text size='xsmall'>
+            Use the rectangle tool to draw a box around the
+            area of the map you'd like to learn more about
+          </Text>
+          <StyledButton
+            color='kelp'
+            gap='xsmall'
+            hoverIndicator={{ color: 'indiglo' }}
+            icon={<Image src={RectangleIcon} />}
+            label={<Text size='xsmall'>RECTANGLE TOOL</Text>}
+            plain
+          />
+        </Box>
 
-      <Box gap='xsmall' margin={{ vertical: 'small' }}>
-        <Text color='kelp'>Dig deeper</Text>
-        <Text size='xsmall'>
-          Toggle additional data layers to see how environmental factors affect
-          kelp forests.
-        </Text>
-        <CheckBox label={<Text color='kelp' size='xsmall'>WIND SPEED</Text>} />
-        <CheckBox label={<Text color='kelp' size='xsmall'>TEMPERATURE DATA</Text>} />
-        <CheckBox label={<Text color='kelp' size='xsmall'>WIND DATA</Text>} />
-        <CheckBox label={<Text color='kelp' size='xsmall'>SATELLITE IMAGERY</Text>} />
-      </Box>
+        <Box gap='xsmall' margin={{ vertical: 'small' }}>
+          <Text color='kelp'>Dig deeper</Text>
+          <Text size='xsmall'>
+            Toggle additional data layers to see how environmental factors affect
+            kelp forests.
+          </Text>
+          <CheckBox label={<Text color='kelp' size='xsmall'>WIND SPEED</Text>} />
+          <CheckBox label={<Text color='kelp' size='xsmall'>TEMPERATURE DATA</Text>} />
+          <CheckBox label={<Text color='kelp' size='xsmall'>WIND DATA</Text>} />
+          <CheckBox label={<Text color='kelp' size='xsmall'>SATELLITE IMAGERY</Text>} />
+        </Box>
 
-      <Box border={{ color: 'kelp', side: 'top' }} gap='xsmall'>
-        <Text color='kelp' margin={{ top: 'xsmall' }}>World map</Text>
-        <Image src={Map} />
+        <Box border={{ color: 'kelp', side: 'top' }} gap='xsmall'>
+          <Text color='kelp' margin={{ top: 'xsmall' }}>World map</Text>
+          <Image src={Map} />
+        </Box>
       </Box>
-    </Box>
+    </aside>
   )
 }
