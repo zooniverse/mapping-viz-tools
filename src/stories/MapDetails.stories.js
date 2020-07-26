00052c54a3b15ed5327ead6a9843846ea6726d32
Will Granger <twgranger@gmail.com>
2020-07-26 10:04:37 -0500

    Create MapDetail with Story

---
+++ src/stories/MapDetails.stories.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import MapDetail from '../components/MapDetail'
+
+storiesOf('MapDetail', module)
+  .add('Default', () => (
+    <MapDetail />
+  ))
