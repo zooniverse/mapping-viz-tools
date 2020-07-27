879aaf49769a67ca2fe881a2a1ccae7914583fbd
Will Granger <twgranger@gmail.com>
2020-07-27 09:01:41 -0500

    Make Slight Modifications to SidePanel

--- src/screens/MapPage/components/SidePanel/SidePanel.js
+++ src/screens/MapPage/components/SidePanel/SidePanel.js
@@ -12,6 +12,10 @@ import Map from 'images/map.png'
 import styled from 'styled-components'
 import LocationDrop from '../LocationDrop'
 
+const Uppercase = styled(Text)`
+  text-transform: uppercase;
+`
+
 const StyledButton = styled(Button)`
   background: white;
   box-shadow: 0px 2.5px 5px gray;
@@ -25,59 +29,58 @@ const StyledText = styled(Text)`
 
 export default function SidePanel() {
   return (
-    <aside>
-      <Box
-        background='sand'
-        border={{ color: 'kelp', side: 'right', size: 'xsmall' }}
-        height={{ min: '100%' }}
-        pad='small'
-        width='15rem'
-      >
-        <Box gap='xsmall' margin={{ bottom: 'small' }} width='10rem'>
-          <Image fit='contain' margin={{ right: 'auto' }} src={Logo} />
-          <StyledText color='kelp' size='xlarge'>
-            Falkland Islands
-          </StyledText>
-          <LocationDrop />
-        </Box>
-
-        <Box
-          border={{ color: 'kelp', side: 'horizontal', size: 'xsmall' }}
-          gap='xsmall'
-          pad={{ vertical: 'small' }}
-        >
-          <Text color='kelp'>Getting started</Text>
-          <Text size='xsmall'>
-            Use the rectangle tool to draw a box around the
-            area of the map you'd like to learn more about
-          </Text>
-          <StyledButton
-            color='kelp'
-            gap='xsmall'
-            hoverIndicator={{ color: 'indiglo' }}
-            icon={<Image src={RectangleIcon} />}
-            label={<Text size='xsmall'>RECTANGLE TOOL</Text>}
-            plain
-          />
-        </Box>
-
-        <Box gap='xsmall' margin={{ vertical: 'small' }}>
-          <Text color='kelp'>Dig deeper</Text>
-          <Text size='xsmall'>
-            Toggle additional data layers to see how environmental factors affect
-            kelp forests.
-          </Text>
-          <CheckBox label={<Text color='kelp' size='xsmall'>WIND SPEED</Text>} />
-          <CheckBox label={<Text color='kelp' size='xsmall'>TEMPERATURE DATA</Text>} />
-          <CheckBox label={<Text color='kelp' size='xsmall'>WIND DATA</Text>} />
-          <CheckBox label={<Text color='kelp' size='xsmall'>SATELLITE IMAGERY</Text>} />
-        </Box>
-
-        <Box border={{ color: 'kelp', side: 'top' }} gap='xsmall'>
-          <Text color='kelp' margin={{ top: 'small' }}>World map</Text>
-          <Image src={Map} />
-        </Box>
+    <Box
+      as='aside'
+      background='sand'
+      border={{ color: 'kelp', side: 'right', size: 'xsmall' }}
+      height={{ min: '100%' }}
+      pad='small'
+      width='15rem'
+    >
+      <Box gap='xsmall' margin={{ bottom: 'small' }} width='10rem'>
+        <Image fit='contain' margin={{ right: 'auto' }} src={Logo} />
+        <StyledText color='kelp' size='xlarge'>
+          Falkland Islands
+        </StyledText>
+        <LocationDrop />
       </Box>
-    </aside>
+
+      <Box
+        border={{ color: 'kelp', side: 'horizontal', size: 'xsmall' }}
+        gap='xsmall'
+        pad={{ vertical: 'small' }}
+      >
+        <Text color='kelp'>Getting started</Text>
+        <Text size='xsmall'>
+          Use the rectangle tool to draw a box around the
+          area of the map you'd like to learn more about
+        </Text>
+        <StyledButton
+          color='kelp'
+          gap='xsmall'
+          hoverIndicator={{ color: 'indiglo' }}
+          icon={<Image src={RectangleIcon} />}
+          label={<Uppercase size='xsmall'>Rectangle Tool</Uppercase>}
+          plain
+        />
+      </Box>
+
+      <Box gap='xsmall' margin={{ vertical: 'small' }}>
+        <Text color='kelp'>Dig deeper</Text>
+        <Text size='xsmall'>
+          Toggle additional data layers to see how environmental factors affect
+          kelp forests.
+        </Text>
+        <CheckBox label={<Uppercase color='kelp' size='xsmall'>Wind Speed</Uppercase>} />
+        <CheckBox label={<Uppercase color='kelp' size='xsmall'>Temperature Data</Uppercase>} />
+        <CheckBox label={<Uppercase color='kelp' size='xsmall'>Wind Data</Uppercase>} />
+        <CheckBox label={<Uppercase color='kelp' size='xsmall'>Satellite Imagery</Uppercase>} />
+      </Box>
+
+      <Box border={{ color: 'kelp', side: 'top' }} gap='xsmall'>
+        <Text color='kelp' margin={{ top: 'small' }}>World map</Text>
+        <Image src={Map} />
+      </Box>
+    </Box>
   )
 }
