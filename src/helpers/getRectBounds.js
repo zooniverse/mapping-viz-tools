7e08931f54a42b54c260f0c40df90b7989fec713
Will Granger <twgranger@gmail.com>
2020-08-20 14:16:30 -0500

    Use build-in leaflet methods to find lat and lon from coords

--- src/helpers/getRectBounds.js
+++ src/helpers/getRectBounds.js
@@ -12,28 +12,12 @@ function getAspectSize(rectangle) {
     return { height, width }
 }
 
-function getUnknownValue(bounds, input, range, minVal, maxVal) {
-    let percent = input / bounds
-    let increment = percent * range
-    return maxVal > minVal ? minVal + increment : minVal - increment
-}
-
 export default function getRectBounds(mapRef, rectangle) {
-    const refDim = mapRef?.current?.container.getBoundingClientRect()
-    const mapBounds = mapRef?.current?.leafletElement.getBounds()
     const aspectSize = getAspectSize(rectangle)
 
-    let mapMinLat = mapBounds._northEast.lat
-    let mapMaxLat = mapBounds._southWest.let
-    let mapMinLng = mapBounds._southWest.lng
-    let mapMaxLng = mapBounds._northEast.lng
-    let latRange = Math.abs(mapBounds._northEast.lat - mapBounds._southWest.lat)
-    let lngRange = Math.abs(mapBounds._northEast.lng - mapBounds._southWest.lng)
+    let map = mapRef?.current?.leafletElement
+    let northEast = map.layerPointToLatLng({ x: rectangle.x + rectangle.width, y: rectangle.y})
+    let southWest = map.layerPointToLatLng({ x: rectangle.x, y: rectangle.y + rectangle.height })
 
-    let minLng = getUnknownValue(refDim.width, rectangle.x, lngRange, mapMinLng, mapMaxLng)
-    let maxLng = getUnknownValue(refDim.width, rectangle.x + rectangle.width, lngRange, mapMinLng, mapMaxLng)
-    let minLat = getUnknownValue(refDim.height, rectangle.y, latRange, mapMinLat, mapMaxLat)
-    let maxLat = getUnknownValue(refDim.height, rectangle.y + rectangle.height, latRange, mapMinLat, mapMaxLat)
-
-    return { minLng, maxLng, minLat, maxLat, ...aspectSize }
+    return { northEast, southWest, ...aspectSize }
 }
