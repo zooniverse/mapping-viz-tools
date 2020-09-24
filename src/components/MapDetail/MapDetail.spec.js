d2d47522a42cc06818a06b6ce3a4f65994253bb7
Will Granger <twgranger@gmail.com>
2020-09-24 10:44:47 -0500

    Update MapDetail Tests

--- src/components/MapDetail/MapDetail.spec.js
+++ src/components/MapDetail/MapDetail.spec.js
@@ -7,23 +7,41 @@ import mockData from './mockData'
 
 describe('Components > MapDetail', function () {
   let wrapper
+  const setActiveSubjectSpy = jest.fn()
+
+  it('should render without props', function () {
+    wrapper = shallow(<MapDetail />)
+    expect(wrapper).toBeDefined()
+  })
 
   beforeEach(function() {
     wrapper = shallow(
-      <MapDetail data={mockData} />
+      <MapDetail data={mockData} setActiveSubject={setActiveSubjectSpy} />
     )
   })
+
+  afterEach(() => jest.clearAllMocks());
   
   it('should render without crashing', function () {
     expect(wrapper).toBeDefined()
   })
 
-  it('should toggle subject visibility', function () {
-    let markers = wrapper.find(Marker)
-    expect(markers.length).toBe(0)
-    let checkBox = wrapper.find(CheckBox).first()
-    checkBox.simulate('change')
-    markers = wrapper.find(Marker)
-    expect(markers.length).toBe(7)
+  describe('subject markers', function () {
+    it('should toggle subject visibility', function () {
+      let markers = wrapper.find(Marker)
+      expect(markers.length).toBe(0)
+      let checkBox = wrapper.find(CheckBox).first()
+      checkBox.simulate('change')
+      markers = wrapper.find(Marker)
+      expect(markers.length).toBe(7)
+    })
+
+    it('should set an active subject', function () {
+      let checkBox = wrapper.find(CheckBox).first()
+      checkBox.simulate('change')
+      let firstMarker = wrapper.find(Marker).first()
+      firstMarker.props().onClick()
+      expect(setActiveSubjectSpy).toHaveBeenCalledWith(mockData[0])
+    })
   })
 })
