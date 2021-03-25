ae99adab1f56de5ec165d0cca7a7932472c5682a
Delilah Clement <delilahclement@gmail.com>
2021-03-25 14:38:15 -0500

    test for 422 status if missing coordinate parameter

--- src/helpers/client.js
+++ src/helpers/client.js
@@ -9,15 +9,20 @@ async function getSubjects(coordinates) {
     const maxLon = northEast.lng
     const endpoint = `${config.dbEndpoint}/subjects?minLat=${minLat}&minLon=${minLon}&maxLat=${maxLat}&maxLon=${maxLon}`
     const response = await fetch(endpoint)
-      .then(response => response.json())
+      .then(response => {
+        if (response.status === 500) {
+          return { statusCode: 500, title: 'Error connecting to database' }
+        }
+        if (response.status === 422) {
+          return { statusCode: 422, title: 'Missing one of parameters: minLat, maxLat, minLon, maxLon' }
+        }
+        if (response.ok) return response.json()
+      })
       .then(data => data)
     return response
   } catch (error) {
-    console.log(error)
+    console.error(error)
   }
 }
 
 export { getSubjects }
-// mapping-viz-functions returns 422 if missing any coordinate parameters
-// responds with 500 if database error
-// could check response is okay in a 200 range
--- src/helpers/client.spec.js
+++ src/helpers/client.spec.js
@@ -3,7 +3,7 @@ import mockData from 'components/MapDetail/mockData'
 import { getSubjects } from './client'
 import { config } from '../config'
 
-describe('fetching subjects data', () => {
+describe('successful response of subjects data', () => {
   const coordinates = {
     northEast: {
       lat: -51.4,
@@ -15,16 +15,16 @@ describe('fetching subjects data', () => {
     },
   }
 
-  const minLat = coordinates.southWest.lat
-  const maxLat = coordinates.northEast.lat
-  const minLon = coordinates.southWest.lng
-  const maxLon = coordinates.northEast.lng
-
   beforeAll(() => {
     const scope = nock(config.dbEndpoint)
       .persist()
       .get(`/subjects`)
-      .query({ minLat, minLon, maxLat, maxLon })
+      .query({
+        minLat: coordinates.southWest.lat,
+        minLon: coordinates.southWest.lng,
+        maxLat: coordinates.northEast.lat,
+        maxLon: coordinates.northEast.lng,
+      })
       .reply(200, mockData, { 'Access-Control-Allow-Origin': '*' })
   })
 
@@ -38,4 +38,42 @@ describe('fetching subjects data', () => {
   })
 })
 
-// add tests for various error responses from mapping-viz-functions
+describe('missing coordinates when fetching subject data', () => {
+  const coordinates = {
+    northEast: {
+      lat: -51.4,
+      lng: -59.5,
+    },
+    southWest: {
+      lat: -52,
+      lng: '',
+    },
+  }
+
+  let response
+  beforeAll(async () => {
+    const scope = nock(config.dbEndpoint)
+      .persist()
+      .get(`/subjects`)
+      .query({
+        minLat: coordinates.southWest.lat,
+        minLon: coordinates.southWest.lng,
+        maxLat: coordinates.northEast.lat,
+        maxLon: coordinates.northEast.lng,
+      })
+      .reply(422, [], { 'Access-Control-Allow-Origin': '*' })
+    response = await getSubjects(coordinates)
+  })
+
+  afterAll(() => {
+    nock.cleanAll()
+  })
+
+  it('should return a 422 code', async () => {
+    expect(response.statusCode).toEqual(422)
+  })
+
+  it('should return a 422 code', async () => {
+    expect(response.title).toEqual('Missing one of parameters: minLat, maxLat, minLon, maxLon')
+  })
+})
