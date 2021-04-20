0994b8ef35cc5d8afcdc35531cb7e1d75c63dd71
Delilah Clement <delilahclement@gmail.com>
2021-04-20 15:53:08 -0500

    fix failing getRectBounds specs

--- src/helpers/getRectBounds.js
+++ src/helpers/getRectBounds.js
@@ -15,9 +15,9 @@ export function getAspectSize(rectangle) {
 export default function getRectBounds(mapRef, rectangle) {
     const aspectSize = getAspectSize(rectangle)
 
-    let map = mapRef.current.leafletElement
-    let northEast = map.containerPointToLatLng({ x: rectangle.x + rectangle.width, y: rectangle.y})
-    let southWest = map.containerPointToLatLng({ x: rectangle.x, y: rectangle.y + rectangle.height })
+    let map = mapRef?.current?.leafletElement
+    let northEast = map?.containerPointToLatLng({ x: rectangle.x + rectangle.width, y: rectangle.y})
+    let southWest = map?.containerPointToLatLng({ x: rectangle.x, y: rectangle.y + rectangle.height })
 
     return { northEast, southWest, ...aspectSize }
 }
