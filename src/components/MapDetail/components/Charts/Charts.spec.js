0bcb0160475f630823780e47217389b22bb4da36
Delilah C <23665803+goplayoutside3@users.noreply.github.com>
2021-05-11 12:31:10 -0500

    Render real data for Temperature vs Time plot (#39)
    * use real data in temperature vs time plot
    
    * adjust tests to new years range
    
    * remove only

--- src/components/MapDetail/components/Charts/Charts.spec.js
+++ src/components/MapDetail/components/Charts/Charts.spec.js
@@ -4,11 +4,11 @@ import Charts from './Charts'
 import Plot from '../Plot'
 import mockData from '../../mockData'
 
-const mockYears = [1985, 1990, 1995, 2000, 2005]
+const mockYears = [1995, 2000, 2005, 2010, 2015]
 
 const expectedAvgData = [
-  { x: 1990, y: 2.5155333333333334 },
-  { x: 2001, y: 978 },
+  { x: 2000, y: 2.5155333333333334 },
+  { x: 2001, y: 9.78 },
   { x: 2013, y: 12.5265 },
   { x: 2014, y: 1.4246 },
   { x: 2016, y: 3.8519 }
@@ -24,7 +24,7 @@ describe('Components > Charts', () => {
 
   it('should calc and pass data averages to corresponding Plot components', () => {
     wrapper = mount(
-      <Charts subjects={mockData} year={1985} years={mockYears} />
+      <Charts subjects={mockData} year={2000} years={mockYears} />
     )
 
     const plot = wrapper.find(Plot).at(0)
