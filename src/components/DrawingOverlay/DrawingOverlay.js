40550431d81bdd59f72db9fb12d3b01d4abbb136
Delilah Clement <delilahclement@gmail.com>
2022-04-21 16:12:49 -0500

    whitespace

--- src/components/DrawingOverlay/DrawingOverlay.js
+++ src/components/DrawingOverlay/DrawingOverlay.js
@@ -5,90 +5,102 @@ import getRectBounds from 'helpers/getRectBounds'
 import { useMap } from 'react-leaflet'
 
 export const SVG = styled.svg`
-	${props => props.canDraw ? css`cursor: crosshair;` : css`cursor: default;`}
+  ${props =>
+    props.canDraw
+      ? css`
+          cursor: crosshair;
+        `
+      : css`
+          cursor: default;
+        `}
   touch-action: none;
 `
 
 export default function DrawingOverlay({
-    canDraw = false,
-    changeDrawing = () => {},
-    setCoords = () => {}
+  canDraw = false,
+  changeDrawing = () => {},
+  setCoords = () => {},
 }) {
-		const mapRef = useMap()
-    const [initCoords, setInitCoords] = React.useState({})
-    const [rectangle, setRectangle] = React.useState(null)
-    const [isDrawing, setIsDrawing] = React.useState(false)
-    const svgRef = React.useRef(null)
-		
-		const getXY = (e) => {
-			const ref = svgRef?.current
-			const boundingBox = ref?.getBoundingClientRect();
-		if (!boundingBox) return
-		let clientX = e?.clientX || 0
-		let clientY = e?.clientY || 0
+  const mapRef = useMap()
+  const [initCoords, setInitCoords] = React.useState({})
+  const [rectangle, setRectangle] = React.useState(null)
+  const [isDrawing, setIsDrawing] = React.useState(false)
+  const svgRef = React.useRef(null)
 
-		const x = (clientX - boundingBox.left)
-		const y = (clientY - boundingBox.top)
-		return { x, y }
-	}
+  const getXY = e => {
+    const ref = svgRef?.current
+    const boundingBox = ref?.getBoundingClientRect()
+    if (!boundingBox) return
+    let clientX = e?.clientX || 0
+    let clientY = e?.clientY || 0
 
-	const onPointerDown = (e) => {
-		if (!canDraw) return
+    const x = clientX - boundingBox.left
+    const y = clientY - boundingBox.top
+    return { x, y }
+  }
 
-		setIsDrawing(true)
-		setInitCoords(getXY(e))
-	}   
+  const onPointerDown = e => {
+    if (!canDraw) return
 
-	const onPointerMove = (e) => {
-		if (!canDraw || !isDrawing) return
-		let newCoords = getXY(e)
-		let width = Math.abs(newCoords.x - initCoords.x) || 0
-		let height = Math.abs(newCoords.y - initCoords.y) || 0
-		let newRect = {
-			width,
-			height,
-			x: Math.min(initCoords.x, newCoords.x),
-			y: Math.min(initCoords.y, newCoords.y)
-		}
-		setRectangle(newRect)
-	}    
+    setIsDrawing(true)
+    setInitCoords(getXY(e))
+  }
 
-	const onPointerUp = (e) => {
-		if (!canDraw || !isDrawing) return
+  const onPointerMove = e => {
+    if (!canDraw || !isDrawing) return
+    let newCoords = getXY(e)
+    let width = Math.abs(newCoords.x - initCoords.x) || 0
+    let height = Math.abs(newCoords.y - initCoords.y) || 0
+    let newRect = {
+      width,
+      height,
+      x: Math.min(initCoords.x, newCoords.x),
+      y: Math.min(initCoords.y, newCoords.y),
+    }
+    setRectangle(newRect)
+  }
 
-		setIsDrawing(false)
-		changeDrawing(false)
-		if (mapRef && rectangle) {
-			setCoords(getRectBounds(mapRef, rectangle))
-		}
-		setRectangle(null)
-	}    
+  const onPointerUp = e => {
+    if (!canDraw || !isDrawing) return
 
-	return (
-		<SVG
-			canDraw={canDraw}
-			onPointerDown={onPointerDown}
-			onPointerMove={onPointerMove}
-			onPointerUp={onPointerUp}
-			ref={svgRef}
-			style={{ width: '100%', height: '100%', position: 'absolute', zIndex: '1000' }}
-		>
-			{rectangle && (
-				<rect
-					x={rectangle.x}
-					y={rectangle.y}
-					width={rectangle.width}
-					height={rectangle.height}
-					fill='transparent'
-					stroke='#AFFF00'
-					strokeWidth='2'
-				/>
-			)}
-		</SVG>
-	)
+    setIsDrawing(false)
+    changeDrawing(false)
+    if (mapRef && rectangle) {
+      setCoords(getRectBounds(mapRef, rectangle))
+    }
+    setRectangle(null)
+  }
+
+  return (
+    <SVG
+      canDraw={canDraw}
+      onPointerDown={onPointerDown}
+      onPointerMove={onPointerMove}
+      onPointerUp={onPointerUp}
+      ref={svgRef}
+      style={{
+        width: '100%',
+        height: '100%',
+        position: 'absolute',
+        zIndex: '1000',
+      }}
+    >
+      {rectangle && (
+        <rect
+          x={rectangle.x}
+          y={rectangle.y}
+          width={rectangle.width}
+          height={rectangle.height}
+          fill='transparent'
+          stroke='#AFFF00'
+          strokeWidth='2'
+        />
+      )}
+    </SVG>
+  )
 }
 
 DrawingOverlay.propTypes = {
-	canDraw: bool,
-	changeDrawing: func,
+  canDraw: bool,
+  changeDrawing: func,
 }
