fae47b89f055b64d9c74c62e4cbe1ae6ed245138
Jim O'Donnell <jim@zooniverse.org>
2022-12-08 13:59:13 +0000

    Mock window.ResizeObserver

--- src/setupTests.js
+++ src/setupTests.js
@@ -6,3 +6,17 @@ import { configure } from 'enzyme'
 import Adapter from '@wojtekmaj/enzyme-adapter-react-17'
 
 configure({ adapter: new Adapter() })
+
+class ResizeObserver {
+  observe() {
+      // do nothing
+  }
+  unobserve() {
+      // do nothing
+  }
+  disconnect() {
+      // do nothing
+  }
+}
+
+global.ResizeObserver = ResizeObserver
